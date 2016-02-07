d9adfa077069760b4135db7c542354ad5bdccf4f
Philip Ullersted <philipullersted@gmail.com>
2016-02-07 22:33:20 +0100

    Added farm and testing for the farm!

---
+++ The RPG thread game/Testing/TestFarm.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using The_RPG_thread_game.Farm_Semphore_;
+using The_RPG_thread_game.Utillity;
+
+namespace Testing
+{
+    [TestClass]
+    public class TestFarm
+    {
+        private int TestTimerID = 5;
+        private int WaitingFarmers = 13;
+
+        private Action<Thread> StartNJoinThread = thread =>
+        {
+            thread.Start();
+            thread.Join();
+        };
+        
+
+        [TestMethod]
+        public void TestEnteringOfFarm()
+        {
+            Farm Farm = new Farm();
+            FarmResourceManager.Instance.Resource = 0;
+            Thread Thread = new Thread(() => FarmThread(Farm));
+
+            StartNJoinThread(Thread);
+
+            Assert.AreEqual(FarmResourceManager.Instance.Resource, Farm.ResourcePerFarmer);
+        }
+
+        [TestMethod]
+        public void TestSemaphoreWaitingWithXAntalFarmers()
+        {
+            Farm Farm = new Farm();
+            FarmResourceManager.Instance.Resource = 0;
+            Thread[] Threads = new Thread[Farm.MaxFarmerCount + WaitingFarmers];
+            
+
+            for (int ThreadNumber = 0; ThreadNumber < Threads.Length; ThreadNumber++)
+            {
+                StartNJoinThread(new Thread(() => FarmThread(Farm)));
+            }
+
+            Assert.AreEqual(FarmResourceManager.Instance.Resource,Threads.Length * Farm.ResourcePerFarmer);
+        }
+
+        private void FarmThread(Farm farm)
+        {
+            farm.Enter();
+        }
+    }
+}
---
+++ The RPG thread game/The RPG thread game/Farm(Semphore)/FarmResourceManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace The_RPG_thread_game.Farm_Semphore_
+{
+    public class FarmResourceManager
+    {
+        public int Resource;
+
+        private static FarmResourceManager MyInstance;
+        private static readonly object SyncLock = new object();
+
+        public static FarmResourceManager Instance
+        {
+            get
+            {
+                lock (SyncLock)
+                {
+                    if (MyInstance == null)
+                    {
+                        MyInstance = new FarmResourceManager();
+                    }
+                    return MyInstance;
+                }
+            }
+        }
+
+        // Private constructor, so that other classes can't create a FarmResourceManager.
+        private FarmResourceManager(){}
+
+
+       
+        
+    }
+}
